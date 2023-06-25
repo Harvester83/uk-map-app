2d78ca01e274754d07e6ee890fb71c890496c0c2
Rashad <rashad.suleymanov83@gmail.com>
2023-06-25 19:17:34 +0400

    add Marker with popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./App.scss";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const center: [number, number] = [54.7023545, -3.2765753];
+
+  const [populationData, setPopulationData] = React.useState(null);
+
+  React.useEffect(() => {
+    const getPopulationData = async () => {
+      const response = await axios.get(
+        `https://sc-test-data-uk.netlify.app/great_britain_1.geojson`
+      );
+      console.log("data: ", response.data);
+
+      setPopulationData(response.data);
+    };
+
+    getPopulationData();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="wrapper">
+      <div className="aside">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum accusamus
+        odio tempore dicta, ullam culpa, tempora dolor neque ducimus harum et.
+        Et corrupti fugit accusamus eos ratione quaerat recusandae aliquid.
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <MapContainer center={center} zoom={6} style={{ flex: "70%" }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="Map data © <a href='https://openstreetmap.org'>OpenStreetMap</a> contributors"
+        />
 
-export default App
+        {populationData && (
+          <GeoJSON
+            data={populationData}
+            style={() => ({
+              fillColor: "#d48585",
+              fillOpacity: 0.6,
+              color: "#000",
+              weight: 1,
+            })}
+            onEachFeature={(feature, layer) => {
+              layer.bindPopup(
+                `<strong>Region name: ${feature.properties.name}</strong><br>Population: ${feature.properties.population}`
+              );
+            }}
+          />
+        )}
+
+        <Marker position={center}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default App;
